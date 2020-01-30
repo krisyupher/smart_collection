29e65a28f54a2ab6dc256622f1df4ee8798ce4ff
krisyupher <krisyupher@gmail.com>
2020-01-29 22:44:24 -0500

    maquetado

---
+++ src/components/Comp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-lg-6">
+    <div class="card">
+      <div class="card-header">
+        <h5 class="box-title">Fullscreen Action</h5>
+        <div class="card-actions">
+          <a class="fullscreen-link">
+            <i class="ti-fullscreen"></i>
+          </a>
+        </div>
+      </div>
+      <div
+        class="card-body"
+      >Sunt exercitation dolor in cillum sed commodo consectetur ea ut labore veniam voluptate est in in non veniam proident in sit ut laboris aliquip veniam minim dolor ut eu ex ullamco duis excepteur commodo dolor sit et aliquip ut qui proident eu in excepteur dolore laboris ad in quis in sint laboris minim elit sed incididunt nulla sit do labore nisi mollit laborum minim duis dolor qui dolor deserunt ut esse ullamco excepteur dolor consectetur duis consectetur ut amet in nostrud exercitation sint irure dolore proident adipiscing ullamco tempor dolore eu elit laborum ut esse est sed cupidatat ad ullamco</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Comp'
+}
+</script>
---
+++ src/views/Componentes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Componentes">
+    <Comp />
+    <Comp />
+    <Comp />
+    <Comp />
+  </div>
+</template>
+
+<script>
+import Comp from '../components/Comp.vue';
+export default {
+  name: 'Componentes',
+  components:{
+    Comp
+  }
+};
+</script>
